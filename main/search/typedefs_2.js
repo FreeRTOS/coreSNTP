33a1850d2d3dfdc5cbaf49cc9bf54048ef362c40
aggarg <aggarg@users.noreply.github.com>
2025-04-21 10:55:47 +0000

    Doxygen for 31dcafa220688df9cd01611da9ef18c180a1cec5

--- main/search/typedefs_2.js
+++ main/search/typedefs_2.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['udptransportrecvfrom_5ft_0',['UdpTransportRecvFrom_t',['../group__sntp__callback__types.html#ga138d8e928110f9b0cbc214135c91b344',1,'core_sntp_client.h']]],
-  ['udptransportsendto_5ft_1',['UdpTransportSendTo_t',['../group__sntp__callback__types.html#gaadd728af5c040252f0b7224e72ea771f',1,'core_sntp_client.h']]],
-  ['unixtime_5ft_2',['UnixTime_t',['../core__sntp__serializer_8h.html#a8877fd3eea1a90efdd3cea1024d5282d',1,'core_sntp_serializer.h']]]
+  ['udptransportsendto_5ft_1',['UdpTransportSendTo_t',['../group__sntp__callback__types.html#gaadd728af5c040252f0b7224e72ea771f',1,'core_sntp_client.h']]]
 ];
